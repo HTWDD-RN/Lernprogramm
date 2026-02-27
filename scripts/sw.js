cb14df90463d37a0c8513a4711d20aaed82f1df9
Jörg Vogt <joerg.vogt@htw-dresden.de>
2026-02-27 08:59:45 +0100

    feat: init 2026

--- scripts/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => event.waitUntil(
-    caches.open('pwa1').then(cache => cache.add('/'))
-));
-
-self.addEventListener('fetch', event => event.respondWith(
-    caches.open('pwa1')
-        .then(cache => cache.match(event.request))
-        .then(response => response || fetch(event.request))
-));
